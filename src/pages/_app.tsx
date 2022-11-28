7df1d51a7bf08a9476e8fd74c7f3f85a1fc196f0
Sam King <mail@samking.co>
2022-11-28 21:14:30 +0000

    Add plausible script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,18 @@
 import "@reach/dialog/styles.css";
 import type { AppProps } from "next/app";
+import Script from "next/script";
 import "../globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Script
+        defer
+        data-domain="samking.photo"
+        src="https://plausible.io/js/plausible.js"
+        strategy="afterInteractive"
+      />
+    </>
+  );
 }
