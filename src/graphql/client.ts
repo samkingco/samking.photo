0b8bf8d8739bba6b6898f07f1369dc167d1e4ab0
Sam King <mail@samking.co>
2022-11-28 11:20:30 +0000

    Remove unused code

--- src/graphql/client.ts
+++
@@ -1,7 +0,0 @@
-import { GraphQLClient } from "graphql-request";
-
-export const graphQlClient = new GraphQLClient(process.env.CMS_ENDPOINT || "", {
-  headers: {
-    Authorization: `Bearer ${process.env.CMS_AUTH || ""}`,
-  },
-});
